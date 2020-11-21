82418ea826b0c2a536e1c984af92570f3d10013f
joker <tom.tim1@mail.ru>
2020-11-21 23:54:53 +0700

    Lesson 4, add sprite

--- PyGame/game.py
+++ PyGame/game.py
@@ -4,17 +4,58 @@ pygame.init()
 win = pygame.display.set_mode((500, 500))  # Creating a game win
 pygame.display.set_caption("Cube Game")  # Call the name of the game
 
-x = 50
-y = 50
-width = 40
-height = 40
+# Sprites for the player's movement
+walkRight = [pygame.image.load('Sprite/right_1.png'), pygame.image.load('Sprite/right_2.png'),
+             pygame.image.load('Sprite/right_3.png'), pygame.image.load('Sprite/right_4.png'),
+             pygame.image.load('Sprite/right_5.png'), pygame.image.load('Sprite/right_6.png')]
+
+walkLeft = [pygame.image.load('Sprite/left_1.png'), pygame.image.load('Sprite/left_2.png'),
+            pygame.image.load('Sprite/left_3.png'), pygame.image.load('Sprite/left_4.png'),
+            pygame.image.load('Sprite/left_5.png'), pygame.image.load('Sprite/left_6.png')]
+
+# Background Image
+bg = pygame.image.load('Sprite/bg.jpg')
+# Sprite when player is standing
+playerStand = pygame.image.load('Sprite/idle.png')
+
+clock = pygame.time.Clock()
+
+x = 5
+y = 424
+width = 60
+height = 71
 speed = 5
+
 isJump = False
 jumpCount = 10
 
+right = False
+left = False
+animCount = 0
+
+
+def drawWindow():
+    global animCount
+
+    win.blit(bg, (0, 0))  # Adding an image to the screen
+
+    if animCount + 1 >= 30:  # The game will need 30 FPS for animation
+        animCount = 0
+
+    if left:  # Playing sprites when moving sideways
+        win.blit(walkLeft[animCount // 5], (x, y))
+        animCount += 1
+    elif right:
+        win.blit(walkRight[animCount // 5], (x, y))
+        animCount += 1
+    else:  # Displaying a standing player
+        win.blit(playerStand, (x, y))
+    pygame.display.update()  # To change the image on screen we need to update it
+
+
 run = True
 while run:
-    pygame.time.delay(50)  # We need a delay so that the loop doesn't run too fast
+    clock.tick(30)  # Setting the game a fixed 30 FPS
 
     # See what events are happening
     for event in pygame.event.get():
@@ -28,14 +69,18 @@ while run:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and x > 0:
         x -= speed
-    if keys[pygame.K_RIGHT] and x < 500 - width:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < 500 - width:
         x += speed
+        left = False
+        right = True
+    else:
+        left = False
+        right = False
+        animCount = 0
 
-    if not isJump:  # During the jump the player can't move UP and DOWN this we need to block it
-        if keys[pygame.K_UP] and y > 0:
-            y -= speed
-        if keys[pygame.K_DOWN] and y < 500 - height:
-            y += speed
+    if not isJump:  # During the jump the player
         if keys[pygame.K_SPACE]:  # Just like with over buttons we also check the SPACE
             isJump = True
     else:
@@ -50,8 +95,6 @@ while run:
             isJump = False
             jumpCount = 10
 
-    win.fill((0, 0, 0))  # Fill the screen with black so that we do not draw but move it
-    pygame.draw.rect(win, (0, 255, 0), (x, y, width, height))  # Creating player
-    pygame.display.update()  # To change the image on screen we need to update it
+    drawWindow()
 
 pygame.quit()  # We turn off program you can only get here if that eternal cycle ends
